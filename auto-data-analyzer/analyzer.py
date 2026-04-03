70e0d8a1808ef50f95f7091e11af9568d8546404
Harshit Sharma <harshittechno01@gmail.com>
2026-04-03 12:08:12 +0530

    Update analyzer.py

--- auto-data-analyzer/analyzer.py
+++ auto-data-analyzer/analyzer.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
 def clean_data(df):
+    numeric_cols = df.select_dtypes(include=['number']).columns
+    categorical_cols = df.select_dtypes(exclude=['number']).columns
 
-    df = df.drop_duplicates()
+    # Fill numeric columns with mean
+    df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
 
-    for col in df.columns:
-
-        if df[col].dtype == "object":
-            df[col] = df[col].fillna("Unknown")
+    # Fill categorical columns with mode
+    for col in categorical_cols:
+        if not df[col].mode().empty:
+            df[col] = df[col].fillna(df[col].mode()[0])
         else:
-            df[col] = df[col].fillna(df[col].mean())
+            df[col] = df[col].fillna("Unknown")
 
     return df
 
